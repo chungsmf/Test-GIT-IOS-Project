eda63a7d89bfa5a09f9e4fd704d826db0d7f7d78
chungsmf <chungsmf@gmail.com>
2014-10-13 12:27:40 -0400

    added a test class

---
+++ SMCTest.h
@@ -0,0 +1,13 @@
+//
+//  SMCTest.h
+//  Git Test
+//
+//  Created by Shi Ming Chung on 10/13/14.
+//  Copyright (c) 2014 Coffee Shop. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SMCTest : NSObject
+
+@end
---
+++ SMCTest.m
@@ -0,0 +1,13 @@
+//
+//  SMCTest.m
+//  Git Test
+//
+//  Created by Shi Ming Chung on 10/13/14.
+//  Copyright (c) 2014 Coffee Shop. All rights reserved.
+//
+
+#import "SMCTest.h"
+
+@implementation SMCTest
+
+@end
